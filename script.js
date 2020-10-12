2aef1894730f157c8cbe5bfc7573bfd5819cdf0d
Gabriel Monteiro Duete <59345698+gabrielduete@users.noreply.github.com>
2020-10-11 23:33:05 -0300

    Update script.js
    Adicionando algumas perguntas ao quiz.

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// Questões do quiz.
+// Questões do quiz;
 quest1 = ["Qual foi o primeiro computador totalmente programável?"]
-quest2 = [" Qual foi o(a) primeiro(a) progamdor(a) do Mundo?"]
+quest2 = ["Qual foi o(a) primeiro(a) progamdor(a) do Mundo?"]
 quest3 = ["Quem é considerado o pai da computação?"]
 quest4 = ["Qual foi o primeiro computador pessoal moderno?"]
-quest5 = [""]
+quest5 = ["Qual computador é conhecido como o melhor computador do mundo?"]
 quest6 = [""]
 quest7 = [""]
 quest8 = [""]
 quest9 = [""]
 quest10 = [""]
 
-//Respostas correspondente a cada questão. -> 
+//Respostas correspondente a cada questão;
 resp1 = ["ENIAC", "EDVAC", "Macintosh", "Apple I"] // CORRETA -> 0
 resp2 = ["Margaret Hamilton","Alan Turing","Condessa de Lovelace","Bill Gates"] // CORRETA -> 2
 resp3 = ["Alan Turing","Bill Gates","Steve Jobs","Charles Babbage"] // Correta -> 0
-resp4 = ["Apple I","IBM 5150","EDVAC",""] //
-resp5 = ["","","",""] //
+resp4 = ["Apple I","IBM 5150","EDVAC",""] // CORRETA -> 3
+resp5 = ["EDVAC","Apple I","Macintosh","Jaguar"] // CORRETA -> 4
 resp6 = ["","","",""] //
 resp7 = ["","","",""] //
 resp8 = ["","","",""] //
 resp9 = ["","","",""] //
 resp10 = ["","","",""] //
 
-//Ordem da Questão /
+//Ordem da Questão;
 ord1 = ["Questão 1/10"]
 ord2 = ["Questão 2/10"]
 ord3 = ["Questão 3/10"]
@@ -34,7 +34,7 @@ ord8 = ["Questão 8/10"]
 ord9 = ["Questão 9/10"]
 ord10 = ["Questão 10/10"]
 
-// Variaveis
+// Variaveis;
 var perg = document.querySelector('h2')
 var ordem = document.querySelector('p')
 var botaom1 = document.querySelector('button#botao1')
@@ -45,8 +45,7 @@ var proxima = document.querySelector('button#ProximaQuest')
 var checaBotaoClicado = false
 
 
-//Funções
-
+//Funções;
 function teste(){
     perg.innerHTML = quest1
     ordem.innerHTML = ord1
@@ -54,7 +53,6 @@ function teste(){
     botaom2.innerHTML = resp1[1]
     botaom3.innerHTML = resp1[2]
     botaom4.innerHTML = resp1[3]
-
 }
 
 function clicouBotao(botaoClicado){
@@ -63,11 +61,14 @@ function clicouBotao(botaoClicado){
     botaom3.value = resp1[2]
     botaom4.value = resp1[3]
     if (botaoClicado.value == resp1[0]){
-        document.querySelector('button#botao1').style.backgroundColor = 'rgb(13, 220, 13)'
+        botaoClicado.style.backgroundColor = 'rgb(13, 220, 13)'
+        botaoClicado.style.color = 'black'
+
     }else{
         botaoClicado.style.backgroundColor = 'red'
+        botaoClicado.style.color = 'black'
     }
     document.getElementById('ProximaQuest').style.visibility = 'visible'
     document.getElementById('ProximaQuest').style.display = 'inline'
+    
 }
-
