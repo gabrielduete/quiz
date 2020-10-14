34433f2a100c0b17d76443b76e08dcdb47b8982d
Gabriel Monteiro Duete <59345698+gabrielduete@users.noreply.github.com>
2020-10-14 15:04:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ quest2 = ["Qual foi o(a) primeiro(a) progamdor(a) do Mundo?"]
 quest3 = ["Quem é considerado o pai da computação?"]
 quest4 = ["Qual foi o primeiro computador pessoal moderno?"]
 quest5 = ["Qual computador é conhecido como o melhor computador do mundo?"]
-quest6 = [""]
-quest7 = [""]
-quest8 = [""]
-quest9 = [""]
-quest10 = [""]
+quest6 = ["Quando lançou o primeiro computador voltado para o uso doméstico?"]
+quest7 = ["Qual a primeira linguagem de programação criada?"]
+quest8 = ["Quem inventou o primeiro compilador?"]
+quest9 = ["Quem inventou o World Wide Web?"]
+quest10 = ["Quem idealizou o sistema Windows?"]
 
 //Respostas correspondente a cada questão;
 resp1 = ["ENIAC", "EDVAC", "Macintosh", "Apple I"] // CORRETA -> 0
 resp2 = ["Margaret Hamilton","Alan Turing","Condessa de Lovelace","Bill Gates"] // CORRETA -> 2
 resp3 = ["Alan Turing","Bill Gates","Steve Jobs","Charles Babbage"] // Correta -> 0
 resp4 = ["Apple I","IBM 5150","EDVAC","Jaguar"] // CORRETA -> 1
-resp5 = ["EDVAC","Apple I","Macintosh","Jaguar"] // CORRETA -> 4
-resp6 = ["","","",""] //
-resp7 = ["","","",""] //
-resp8 = ["","","",""] //
-resp9 = ["","","",""] //
-resp10 = ["","","",""] //
+resp5 = ["EDVAC","Apple I","Macintosh","Jaguar"] // CORRETA -> 3
+resp6 = ["1999","1976","1981","2000"] // CORRETA -> 2
+resp7 = ["Plankalkül","COBOL","Fortran","Pascal"] // CORRETA -> 0
+resp8 = ["Margaret Hamilton","Grace Hopper","Alan Turing","Charles Babbage"] // CORRETA -> 1
+resp9 = ["Alan Turing","Bill Gates","Tim Berners-Lee","Steve Jobs"] // CORRETA -> 2
+resp10 = ["Bill Gates","Gabriel Duete","Steve Jobs","Charles Babbage"] // CORRETA -> 1
 
 //Ordem da Questão;
 ord1 = ["Questão 1/10"]
@@ -34,7 +34,7 @@ ord8 = ["Questão 8/10"]
 ord9 = ["Questão 9/10"]
 ord10 = ["Questão 10/10"]
 
-sequenciaResposta = [ 0, 2, 0, 1, 4 ]
+sequenciaResposta = [ 0, 2, 0, 1, 3, 2, 0, 1, 2, 0]
 
 // Variaveis;
 var perg = document.querySelector('h2')
@@ -45,7 +45,7 @@ var botaom3 = document.querySelector('button#botao3')
 var botaom4 = document.querySelector('button#botao4')
 var proxima = document.querySelector('button#ProximaQuest')
 var index = document.querySelector('input#index')
-
+var contador = 0
 
 // Mudança inicial;
 perg.innerHTML = quest1
@@ -63,12 +63,10 @@ function clicouBotao(botaoClicado) {
     if (botaoClicado.innerHTML == window["resp" + indexNumber][indexAnswer]){
         botaoClicado.style.backgroundColor = 'rgb(13, 220, 13)'
         botaoClicado.style.color = 'black'
-        console.log('acertou')
-
+        contador += 1
     }else{
         botaoClicado.style.backgroundColor = 'red'
         botaoClicado.style.color = 'black'
-        console.log('teste')
     }
 
     document.getElementById('ProximaQuest').style.visibility = 'visible'
